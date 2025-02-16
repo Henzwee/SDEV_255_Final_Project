7cc6104d138b3dcfb468bad2a0b0ce6282148712
Henzwee <166328821+Henzwee@users.noreply.github.com>
2025-02-16 11:47:42 -0500

    Update styles.js

--- styles.js
+++ styles.js
@@ -1,6 +1,1 @@
-    // Show/hide links based on the user role
-    if (userRole === "student") {
-      document.getElementById("add-courses-link").style.display = "block";
-    } else if (userRole === "teacher") {
-      document.getElementById("create-courses-link").style.display = "block";
-    }
+
