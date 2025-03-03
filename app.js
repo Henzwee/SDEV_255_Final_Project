09f8ca582bf51de11baab5783b3421baaeef277a
Corey Lambert <157275713+CsTL0522@users.noreply.github.com>
2025-03-03 16:39:30 -0600

    changes to paths

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, )));
+app.use(express.static(__dirname, ));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Serve index.html for the root route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 
 // Routes for handling form submissions and redirecting to static pages
@@ -42,15 +42,15 @@ app.post('/login', (req, res) => {
 
 // Serve other static HTML files
 app.get('/add_courses.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'add_courses.html'));
+    res.sendFile(path.join(__dirname, 'add_courses.html'));
 });
 
 app.get('/create_courses.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'create_courses.html'));
+    res.sendFile(path.join(__dirname, 'create_courses.html'));
 });
 
 app.get('/schedule.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'schedule.html'));
+    res.sendFile(path.join(__dirname, 'schedule.html'));
 });
 
 //app.listen(3000, () => {
